5f292bb5d03258942310488cad9ce390c0c1621a
Olaf van Wijk <o_van_wijk@hotmail.com>
2025-10-16 14:30:10 +0200

    points

---
+++ packages/client-sdk/src/api-endpoints/index.ts
@@ -0,0 +1,8 @@
+const API_PATHS = {
+    GET_PROCESSORS: "api/get-processors",
+    GET_DATA_STREAMS: "api/get-datastreams",
+    GET_PROCESSOR_TOKEN: "api/get-processor-token",
+    GET_DATA_STREAM_TOKEN: "api/get-datastream-token"
+}
+
+export { API_PATHS };
